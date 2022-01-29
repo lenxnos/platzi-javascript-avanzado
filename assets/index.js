eb2e2d5c55f631266586add9cbf3d36a32d0da9b
Nelson Soto <nelson.soto.c05@gmail.com>
2022-01-29 12:25:24 -0400

    feat: Add first plugin

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-const player = new MediaPlayer({ el: video });
-button.onclick = () => player.tooglePlay();
+const playButton = document.querySelector("#playButton");
+playButton.onclick = () => player.tooglePlay();
+
+const muteButton = document.querySelector("#muteButton");
+muteButton.onclick = () => player.toogleMute();
