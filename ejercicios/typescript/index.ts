6f4d69f8e91075acab2761262fe556a1932b6079
Nelson Soto <nelson.soto.c05@gmail.com>
2022-01-31 00:29:36 -0400

    feat: Add basic types typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,8 +1,40 @@
-console.log('Hello, TypeScript!');
+// Boolean
+let muted: boolean = true;
+muted = false;
 
-function add(a: number, b: number): number {
-    return a + b;
+// Números 
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = "Richard";
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ['Isabel', 'Nicole', 'Raul'];
+// people.push('Juan');
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push('Paul');
+peopleAndNumbers.push(9001);
+
+// Enum
+
+enum Color {
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul"
 }
 
-const sum = add(1, 2);
-console.log(sum);
+let colorFavorito: Color = Color.Azul;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = "Joker";
+comodin = { type: 'Wildcard' };
+
+// Object
+let someObject: object = { type: 'Wildcard' };
