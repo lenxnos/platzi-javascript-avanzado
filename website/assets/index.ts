82509a0a2e858ecbcdd82aaf6809c0310aad20c9
Nelson Soto <nelson.soto.c05@gmail.com>
2022-02-06 21:43:39 -0400

    feat: Configure project to production

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -1,7 +1,7 @@
-import MediaPlayer from './MediaPlayer';
-import AutoPlay from './plugins/AutoPlay';
-import AutoPause from './plugins/AutoPause';
-import Ads from './plugins/Ads';
+import MediaPlayer from '@lenxnos/platzimediaplayer/lib/MediaPlayer';
+import AutoPlay from '@lenxnos/platzimediaplayer/lib/plugins/AutoPlay';
+import AutoPause from '@lenxnos/platzimediaplayer/lib/plugins/AutoPause';
+import Ads from '@lenxnos/platzimediaplayer/lib/plugins/Ads';
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause(), new Ads()] });
